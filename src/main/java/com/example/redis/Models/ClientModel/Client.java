40751a0f2856671e1a65ba63ddd0b08b9c7bbe2d
nzivo <nzivojohn@students.must.ac.ke>
2019-09-09 12:37:55 +0300

    serialVersionUID Update

--- src/main/java/com/example/redis/Models/ClientModel/Client.java
+++ src/main/java/com/example/redis/Models/ClientModel/Client.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 @Data
 @Table(name = "client")
 public class Client implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -1L;
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     @Column(name="id")
--- src/main/java/com/example/redis/Models/CountryModel/Country.java
+++ src/main/java/com/example/redis/Models/CountryModel/Country.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 @Data
 @Table(name = "country")
 public class Country implements Serializable {
-    private static final long serialVersionUID = -6791377053404789458L;
+    private static final long serialVersionUID = -1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
